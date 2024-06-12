e14167b3defd0858eed7cc4731b81947bfb3b8c8
G384892023 <238489@st.takushoku-u.ac.jp>
2024-06-12 20:04:30 +0900

    A

--- gourmet.js
+++ gourmet.js
@@ -208,7 +208,19 @@ clearBtn.addEventListener('click',clear);
 
 function clear(){
   var clist =document.getElementsByClassName("check_box");
+  console.log(clist.length);
   for (var i = 0; i < clist.length; ++i) {
     clist[i].checked = false; 
+    console.log(clist);
   }
+}
+
+const chooseBtn = document.getElementById('chooseBtn');
+chooseBtn.addEventListener('click',showSelectResult);
+
+function showError() {
+  console.log(err);
+}
+function finish() {
+  console.log('Ajax 通信が終わりました');
 }
